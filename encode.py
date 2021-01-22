b851f3873a289705805cc38d1bdc2d3bc4847e09
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-01-22 23:59:37 +0200

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,5 +1,7 @@
 import compileall
-
+import os
+os.system("clear")
+print ("      Welcome   user        ")
 encrypt = raw_input ("enrypt : ")
 #compileall.compile_dir('')
 A1 = compileall.compile_dir(encrypt)
